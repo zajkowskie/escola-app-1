9f2934f1860d71c36009f40612ddeb5c7e286cf5
Radek Zajkowski <zajkos@MacBook-Pro-Radek.local>
2020-02-20 10:04:36 +0100

    Updates

---
+++ precache-manifest.36857ee3f11b90339c85432ff9f176f0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5ad88026f1c355fb7c46bfa39f61aa",
+    "url": "/escola-app-1/index.html"
+  },
+  {
+    "revision": "bf9f46f23c7b025aeed8",
+    "url": "/escola-app-1/static/css/main.697d3722.chunk.css"
+  },
+  {
+    "revision": "19a141d04a2b2ebeaca6",
+    "url": "/escola-app-1/static/js/2.2995d594.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/escola-app-1/static/js/2.2995d594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9f46f23c7b025aeed8",
+    "url": "/escola-app-1/static/js/main.ffaef5e3.chunk.js"
+  },
+  {
+    "revision": "fb228b7228b20b452a8e",
+    "url": "/escola-app-1/static/js/runtime-main.c4f3aa91.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/escola-app-1/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/escola-app-1/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "e644d03d8f230203aacdb05b3150e6be",
+    "url": "/escola-app-1/static/media/Yrsa-Medium.e644d03d.ttf"
+  },
+  {
+    "revision": "2e1f5e040ba54278e31a175398027034",
+    "url": "/escola-app-1/static/media/Yrsa-Regular.2e1f5e04.ttf"
+  }
+]);
